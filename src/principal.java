19c15ddc2f87dd749fa4b7b69ba30a010f4e4de0
ViniciusSantos19 <vinicontatosq@gmail.com>
2022-09-21 02:02:45 -0300

    teste5

--- src/principal.java
+++ src/principal.java
@@ -6,11 +6,14 @@ public class principal {
 
 	public static void main(String[] args) {
 		Pesquisador Manuel = new Pesquisador("Manuel", null);
+		Pesquisador Renato = new Pesquisador("Renato", Manuel);
 		ProjetoDePesquisaCordenador teste = new ProjetoDePesquisaCordenador(Manuel);
+		ProjetoDePesquisaProfessor teste2 = new ProjetoDePesquisaProfessor(Renato);
 		teste.inserirPesquisadores(new Pesquisador("nome1", Manuel));
 		teste.inserirPesquisadores(new Pesquisador("nome2", Manuel));
 		teste.inserirPesquisadores(new Pesquisador("nome3", Manuel));
 		System.out.println(teste.getValorPago());
+		System.out.println(teste2.getValorPago());
 
 	}
 
