defea7fd99dcd3d9eb5e8bd819b5d107b33dc4bd
ViniciusSantos19 <vinicontatosq@gmail.com>
2023-01-02 22:35:40 -0300

    adicionei classe jogador

--- QuebraCabeca15/src/modelo/Jogador.java
+++ QuebraCabeca15/src/modelo/Jogador.java
@@ -1,5 +1,46 @@
 package modelo;
 
 public class Jogador {
+	private String nome;
+	public Jogo jogo;
+	private double pontos;
+	private int rank;
+	
+	
+	
+	public Jogo getTabuleiro() {
+		return jogo;
+	}
 
+	public void setTabuleiro(int[][] Copiatabuleiro) {
+		for(int i = 0; i < 4; i++) {
+			for(int j = 0; j < 4; j++) {
+				jogo.tabuleiro[i][j] = Copiatabuleiro[i][j];
+			}
+		}
+	}
+
+	public double getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(double pontos) {
+		this.pontos = pontos;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	private String getNome() {
+		return this.nome;
+	}
 }
